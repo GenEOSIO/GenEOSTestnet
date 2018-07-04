2b83af41697d2dc4863aaec2f69b40c8655e920e
GenEOSIO <38351005+GenEOSIO@users.noreply.github.com>
2018-07-04 15:46:18 +0300

    Update clgeneos.sh

--- clgeneos.sh
+++ clgeneos.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-################################################################################
-#
-# Scrip Created by http://CryptoLions.io
-# https://github.com/CryptoLions/scripts/
-#
-###############################################################################
 
 NODEOSBINDIR="/home/ivan/geneos/build/programs"
 
